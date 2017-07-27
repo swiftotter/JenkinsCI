339915302c96359c7111c17c9f5a3d30caf339a1
Joseph Maxwell <joseph@swiftotter.com>
2017-07-27 11:23:26 -0500

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,6 @@
 def call(String nodeName = 'test', String composerAuth = 'unset') {
+    def utilities = new org.swiftotter.Test()
+    
     node (nodeName) {
         withEnv(["COMPOSER_AUTH=" + composerAuth]) {
             milestone 1
@@ -22,39 +24,8 @@ def call(String nodeName = 'test', String composerAuth = 'unset') {
             }
         }
 
-        updateGithubCommitStatus(currentBuild);
+        test.updateGithubCommitStatus(currentBuild);
     }
 
     milestone 2
-}
-
-def getRepoURL() {
-    sh "git config --get remote.origin.url > .git/remote-url"
-    return readFile(".git/remote-url").trim()
-}
-
-def getCommitSha() {
-    sh "git rev-parse HEAD > .git/current-commit"
-    return readFile(".git/current-commit").trim()
-}
-
-def updateGithubCommitStatus(build) {
-  // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
-  repoUrl = getRepoURL()
-  commitSha = getCommitSha()
-
-  step([
-    $class: 'GitHubCommitStatusSetter',
-    reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
-    commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
-    errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-    statusResultSource: [
-      $class: 'ConditionalStatusResultSource',
-      results: [
-        [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description],
-        [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: build.description],
-        [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
-      ]
-    ]
-  ])
 }
