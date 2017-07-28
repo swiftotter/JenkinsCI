20dda6c5f4d4e26289634eb651c1f2bb0227c739
Joseph Maxwell <joseph@swiftotter.com>
2017-07-27 19:30:53 -0500

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,11 +2,10 @@ def call(String nodeName = 'build') {
     node(nodeName) {
         stage ('\u26CF Checkout') {
             checkout scm
-            println 'aws s3 cp ' + OUTPUT_FILE + ' s3://' + S3_DEST_BUCKET + '/jobs/' + JOB_NAME + '/' + BUILD_NUMBER + '/' + OUTPUT_FILE
         }
 
         stage ('\u267B Build') {
-            withCredentials([string(credentialsId: PACKAGIST, variable: 'COMPOSER_AUTH')]) {
+            withCredentials([string(credentialsId: params.PACKAGIST, variable: 'COMPOSER_AUTH')]) {
                 withEnv(["COMPOSER_AUTH=" + COMPOSER_AUTH]) {
                     sh 'rm -rf scripts'
                     sh 'git clone https://github.com/SwiftOtter/Magento1CI.git scripts'
@@ -23,7 +22,7 @@ def call(String nodeName = 'build') {
 
 
         stage('\u2795 Pushing Artifacts') {
-            sh 'aws s3 cp ' + OUTPUT_FILE + ' s3://' + S3_DEST_BUCKET + '/jobs/' + JOB_NAME + '/' + BUILD_NUMBER + '/' + OUTPUT_FILE
+            sh 'aws s3 cp ' + OUTPUT_FILE + ' s3://' + params.S3_DEST_BUCKET + '/jobs/' + JOB_NAME + '/' + BUILD_NUMBER + '/' + OUTPUT_FILE
             currentBuild.result = "SUCCESS"
         }
     }
