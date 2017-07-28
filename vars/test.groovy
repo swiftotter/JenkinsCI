8ea76b5d962d900079d35bb42f538bee9fb4fb17
Joseph Maxwell <joseph@swiftotter.com>
2017-07-27 19:30:17 -0500

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -13,10 +13,10 @@ def call(String nodeName = 'test', String composerAuth = 'unset') {
                 sh 'git clone https://github.com/SwiftOtter/Magento1CI.git scripts'
 
                 sh 'sudo chmod --recursive +x scripts/'
-                withCredentials([string(credentialsId: PACKAGIST, variable: 'COMPOSER_AUTH')]) {
-                    withCredentials([string(credentialsId: CRYPT_KEY, variable: 'CRYPT_KEY')]) {
-                        println 'Running: sh scripts/test.sh'+' -i '+env.BUILD_NUMBER+' -c '+CRYPT_KEY+' -t '+TABLE_PREFIX+' -b '+S3_DB_BUCKET+' -p . -f '+S3_DB_FILE
-                        sh 'scripts/test.sh'+' -i '+env.BUILD_NUMBER+' -c '+CRYPT_KEY+' -t '+TABLE_PREFIX+' -b '+S3_DB_BUCKET+' -p . -f '+S3_DB_FILE
+                withCredentials([string(credentialsId: params.PACKAGIST, variable: 'COMPOSER_AUTH')]) {
+                    withCredentials([string(credentialsId: params.CRYPT_KEY, variable: 'CRYPT_KEY')]) {
+                        println 'Running: sh scripts/test.sh'+' -i '+env.BUILD_NUMBER+' -c '+params.CRYPT_KEY+' -t '+params.TABLE_PREFIX+' -b '+params.S3_DB_BUCKET+' -p . -f '+params.S3_DB_FILE
+                        sh 'scripts/test.sh'+' -i '+env.BUILD_NUMBER+' -c '+params.CRYPT_KEY+' -t '+params.TABLE_PREFIX+' -b '+params.S3_DB_BUCKET+' -p . -f '+params.S3_DB_FILE
                     }
                 }
             }
