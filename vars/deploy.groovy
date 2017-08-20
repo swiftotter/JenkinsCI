3d9776abbec3715895e5160ac152a6bf0ba5249d
Joseph Maxwell <joseph@swiftotter.com>
2017-08-19 20:04:26 -0500

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,6 +2,6 @@ def call(String nodeName = 'deploy', String environment, ArrayList targets) {
     def utilities = new org.swiftotter.Deploy()
     
     targets.each {
-        utilities.deployWithDetails(nodeName, environment, S3_DEST_BUCKET, env.JOB_NAME, env.BUILD_NUMBER, OUTPUT_FILE, it);
+        utilities.deployWithDetails(nodeName, environment, S3_DEST_BUCKET, env.JOB_NAME, env.BUILD_NUMBER, OUTPUT_FILE, params.MAGENTO_VERSION, it);
     }
 }
