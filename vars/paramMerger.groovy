fbbf61243b13153697d4f5449805a54d62dfab73
Joseph Maxwell <joseph@swiftotter.com>
2017-08-22 15:54:41 -0500

    Update paramMerger.groovy

--- vars/paramMerger.groovy
+++ vars/paramMerger.groovy
@@ -35,7 +35,7 @@ def call(body) {
               println "it.getArguments.name = " + it.getArguments().name
               println "param.getArguments.name = " + param.getArguments().name
               
-              return it.getArguments().name == param.getArguments().name }
+              return it.getArguments().name == param.getArguments().name ? true : null }
         
           println value.toString();
           if (!value) {
