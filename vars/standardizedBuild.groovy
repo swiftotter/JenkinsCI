51d0a9c10d100f5b003ca436d5484bd0dd9a5080
Joseph Maxwell <joseph@swiftotter.com>
2017-09-01 19:24:56 -0500

    Fixing syntax error

--- vars/standardizedBuild.groovy
+++ vars/standardizedBuild.groovy
@@ -14,7 +14,7 @@ def call(String nodeName = 'build') {
                 composerAuth = '';
             }
             
-            withCredentials([string(credentialsId: composerAuth variable: 'COMPOSER_AUTH')]) {
+            withCredentials([string(credentialsId: composerAuth, variable: 'COMPOSER_AUTH')]) {
                 withEnv(["COMPOSER_AUTH=" + COMPOSER_AUTH]) {
                     sh 'rm -rf scripts'
                     sh 'git clone https://github.com/SwiftOtter/MagentoCI.git scripts'
