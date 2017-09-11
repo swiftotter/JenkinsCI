f9aec393909afedb0bb3e8fee7c49ddc0d7e506e
Joseph Maxwell <joseph@swiftotter.com>
2017-09-11 08:45:10 -0500

    Adding table prefix argument

--- vars/test.groovy
+++ vars/test.groovy
@@ -18,8 +18,12 @@ def call(String nodeName = 'test', String composerAuth = 'unset') {
                 sh 'sudo chmod --recursive +x scripts/'
                 withCredentials([string(credentialsId: params.PACKAGIST, variable: 'COMPOSER_AUTH')]) {
                     withCredentials([string(credentialsId: params.CRYPT_KEY, variable: 'CRYPT_KEY')]) {
+                        tablePrefixArg = ''
+                        if (params.TABLE_PREFIX) {
+                            tablePrefixArg = '--tablePrefix '+params.TABLE_PREFIX 
+                        }
                         println 'Running: sh scripts/test.sh'+' -i '+env.BUILD_NUMBER+' -c '+params.CRYPT_KEY+' -t '+params.TABLE_PREFIX+' -b '+params.S3_DB_BUCKET+' -p . -f '+params.S3_DB_FILE
-                        sh 'scripts/test.sh --magentoVersion ' + params.MAGENTO_VERSION + ' --theme ' + params.THEME + ' --buildId ' + BUILD_NUMBER + ' --cryptKey '+params.CRYPT_KEY+' --tablePrefix '+params.TABLE_PREFIX+' --s3Bucket '+params.S3_DB_BUCKET+' --filePath . --dbFile '+params.S3_DB_FILE
+                        sh 'scripts/test.sh --magentoVersion ' + params.MAGENTO_VERSION + ' --theme ' + params.THEME + ' --buildId ' + BUILD_NUMBER + ' --cryptKey '+params.CRYPT_KEY+' ' + tablePrefixArg + ' --s3Bucket '+params.S3_DB_BUCKET+' --filePath . --dbFile '+params.S3_DB_FILE
                     }
                 }
             }
